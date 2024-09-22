4ed236eff2646f03c11b79d7c9f820ea51179857
Androsricardo <62420260+Androsricardo@users.noreply.github.com>
2024-09-21 21:04:28 -0600

    Add files via upload

---
+++ generador de contraseña.js
@@ -0,0 +1,41 @@
+let cantidad = document.getElementById('cantidad');
+let boton = document.getElementById('generar');
+let contrasena = document.getElementById('contrasena');
+const cadenaCaracteres = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuwxyz0123456789!@#$%^&*()';
+function generar() {
+    let numeroDigitado = parseInt(cantidad.value);
+    if (numeroDigitado < 8) {
+       
+        alert("La cantidad de caracteres tiene que ser mayor que 8");
+    }
+    let password = '';
+    for (let i = 0; i < numeroDigitado; i++) {
+        let caracterAleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
+        console.log(caracterAleatorio);
+        password += caracterAleatorio;
+    }
+    contrasena.value = password;
+    validarFortaleza(password);
+}
+function validarFortaleza(password) {
+    let mensaje = '';
+    const tieneMayuscula = /[A-Z]/.test(password);
+    const tieneNumero = /[0-9]/.test(password);
+    const tieneSimbolo = /[!@#$%^&*()]/.test(password);
+    if (!tieneMayuscula || !tieneNumero || !tieneSimbolo) {
+        mensaje = 'Contraseña débil:  al menos una letra mayúscula, número y símbolo especial.';
+        alert(mensaje);
+    } else {
+        mensaje = 'Contraseña adecuada ';
+        alert(mensaje);
+    }
+}
+function limpiar() {
+    contrasena.value = '';
+}
+
+
+
+
+
+ 
